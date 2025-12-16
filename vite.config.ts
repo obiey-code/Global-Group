7bcd6f1ba862df00f7e83a8e0c04f4247d50fa71
NOISIM-Engineering-Services <noisimengineering@gmail.com>
2025-12-16 20:55:06 +0100

    supprime

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Le nom de votre dépôt est 'Global-Group'
+// CLÉ CRITIQUE pour GitHub Pages
+const REPO_NAME = 'Global-Group'; 
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  // ----------------------------------------------------
+  // CORRECTION IMPORTANTE POUR GITHUB PAGES
+  // Vite doit savoir où se trouve le dossier du projet sur le domaine (https://obiey-code.github.io/Global-Group/)
+  base: `/${REPO_NAME}/`, 
+  // ----------------------------------------------------
+  
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  
+  // Si vous utilisez TypeScript pour votre projet
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  }
 });
